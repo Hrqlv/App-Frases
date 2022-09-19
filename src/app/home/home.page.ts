5306aadb10617c558707b6228a7a0e570cfe32a6
Henrique Lopes Velozo <henriquelopesvelozo@gmail.com>
2022-09-19 17:59:10 -0300

    App-frases

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+interface Frase {
+  id: number;
+  frase: string;
+  autor: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  url = "http://lucasreno.kinghost.net/frase";
+  frase: Frase = {id: 0, frase: "", autor: ""};
+  
+  constructor(private http: HttpClient) {
+    this.pegarFrase();
+  }
+  pegarFrase(){
+    this.http.get<any>(this.url).subscribe(
+    resposta => {
+      this.frase = resposta[0];
+      
+    }
+  )
+  }
+}
